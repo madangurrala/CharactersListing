e430966f853d745ca3cee0563b271e5057ecf009
Madan <madangurrala@gmail.com>
2019-12-12 20:37:09 -0500

    AddedItemsToDisplay

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  character;
+  @Input() character;
+  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAssign(charSide){
+    // this.character.side = side;
+    this.sideAssigned.emit({name: this.character.name, side: charSide});
+
   }
 
 }
--- src/app/tabular/tabular.component.ts
+++ src/app/tabular/tabular.component.ts
@@ -10,10 +10,33 @@ export class TabularComponent implements OnInit {
     {name: 'Luke SkyWalker', side: ''},
     {name: 'Darth Wander', side: ''}
   ];
+  chosenList = 'all';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getCharacter() {
+
+    if(this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    });
+  }
+
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  onSideChoosen(charInfo){
+    const position = this.characters.findIndex((char) => {
+      return char.name === charInfo.name;
+    });
+    this.characters[position].side = charInfo.side;
+
   }
 
 }
